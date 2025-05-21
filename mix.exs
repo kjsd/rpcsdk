10fb02006d6be8e32ceba48554dd791e81f311f3
Kenji MINOURA <info@kandj.org>
2025-05-21 20:38:19 +0900

    state

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Rpcsdk.MixProject do
   def project do
     [
       app: :rpcsdk,
-      version: "0.4.8",
+      version: "0.4.9",
       elixir: "~> 1.14",
       description: @description,
       package: package(),
