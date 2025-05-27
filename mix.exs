1eb0e4fa95f683c606dcf9caaa308ac428e2ccd2
Kenji MINOURA <info@kandj.org>
2025-05-27 19:21:23 +0900

    refactor

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Rpcsdk.MixProject do
   def project do
     [
       app: :rpcsdk,
-      version: "0.4.10",
+      version: "0.4.11",
       elixir: "~> 1.14",
       description: @description,
       package: package(),
